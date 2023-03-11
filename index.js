4f4746664e46d4dee30b3f6bc8c974dd37379bdd
Kerwin <kerjemtec@gmail.com>
2023-03-11 10:03:56 -0400

    cors

--- index.js
+++ index.js
@@ -21,9 +21,20 @@ const allowedOrigins = [
 
 const app = express();
 app.use(helmet());
-// app.use(cors({ origin: allowedOrigins }));
-
-app.use(cors(*));
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      // allow requests with no origin (like mobile apps or curl requests)
+      if (!origin) return callback(null, true);
+      if (allowedOrigins.indexOf(origin) === -1) {
+        var msg =
+          "The CORS policy for this site does not allow access from the specified Origin.";
+        return callback(new Error(msg), false);
+      }
+      return callback(null, true);
+    },
+  })
+);
 app.use(express.json());
 
 // Route to get all posts
