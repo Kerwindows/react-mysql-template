bb410ab357c9f2e48b6e0843bd58f02cf17eb649
Kerwin <kerjemtec@gmail.com>
2023-03-11 10:11:19 -0400

    fixes

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const db = require("./config/db");
-const helmet = require("helmet");
 const cors = require("cors");
 const {
   getUsers,
@@ -13,28 +12,10 @@ const {
   updateBio,
 } = require("./models");
 
-const allowedOrigins = [
-  "http://localhost:3001",
-  "https://react-mysql.kerwindows.com",
-  "https://www.react-mysql.kerwindows.com",
-];
-
 const app = express();
-app.use(helmet());
-app.use(
-  cors({
-    origin: function (origin, callback) {
-      // allow requests with no origin (like mobile apps or curl requests)
-      if (!origin) return callback(null, true);
-      if (allowedOrigins.indexOf(origin) === -1) {
-        var msg =
-          "The CORS policy for this site does not allow access from the specified Origin.";
-        return callback(new Error(msg), false);
-      }
-      return callback(null, true);
-    },
-  })
-);
+
+const PORT = 3000;
+app.use(cors());
 app.use(express.json());
 
 // Route to get all posts
