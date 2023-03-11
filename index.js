f35a423f3e374b33b155e84e22312e00ffdda97c
Kerwin <kerjemtec@gmail.com>
2023-03-11 10:25:59 -0400

    index.js fixes

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ require("dotenv").config();
 const express = require("express");
 const helmet = require("helmet");
 const cors = require("cors");
-const auth = require("./middleware/auth");
 const db = require("./config/db");
 const {
   getUsers,
@@ -17,122 +16,22 @@ const {
 
 const allowedOrigins = [
   "http://localhost:3001",
-  "https://kerwindows.students.nomoredomainssbs.ru",
-  "https://www.kerwindows.students.nomoredomainssbs.ru",
+  "https://react-mysql.kerwindows.com",
+  "https://www.react-mysql.kerwindows.com",
 ];
 
 const app = express();
 app.use(helmet());
 app.use(cors({ origin: allowedOrigins }));
 app.use(express.json());
-app.use(auth);
+
 // Route to get all posts
 app.get("/api/users", async (req, res) => {
+  res.header("Access-Control-Allow-Origin", "*"); // Add the CORS header
   await getUsers()
     .then((result) => res.status(200).send(result))
     .catch((err) => {
       res.status(400).send({ error: `no records found - ${err}` });
-    });
-});
-
-app.get("/api/user/:username", async (req, res) => {
-  const username = req.params.username;
-  await getUser(username)
-    .then((result) => {
-      res.status(200).send(result);
-    })
-    .catch((err) => {
-      if (err) {
-        res.status(400).send({ error: `${err.message}` });
-      } else {
-        res.status(500).send({ error: "internal server error" });
-      }
-    });
-});
-app.get("/api/links/:id", async (req, res) => {
-  const id = req.params.id;
-  await getLinks(id)
-    .then((result) => {
-      res.status(200).send(result);
-    })
-    .catch((err) => {
-      if (err) {
-        res.status(400).send({ error: `${err.message}` });
-      } else {
-        res.status(500).send({ error: "internal server error" });
-      }
-    });
-});
-app.get("/api/files/:id", async (req, res) => {
-  const id = req.params.id;
-  await getFiles(id)
-    .then((result) => {
-      res.status(200).send(result);
-    })
-    .catch((err) => {
-      if (err) {
-        res.status(400).send({ error: `${err.message}` });
-      } else {
-        res.status(500).send({ error: "internal server error" });
-      }
-    });
-});
-// Route for creating the post
-app.post("/api/create", async (req, res) => {
-  const data = req.body;
-  await regUser(data)
-    .then((result) => res.send(result))
-    .catch((err) => {
-      if (err) {
-        res.status(400).send({ message: "invalid user data" - `${err}` });
-      } else {
-        res.status(500).send({ Message: "internal error" });
-      }
-    });
-});
-
-// Route for like
-app.patch("/api/bio/:username", async (req, res) => {
-  const username = req.params.username;
-  const data = req.body;
-  await updateBio(data, username)
-    .then((user) => res.send({ data: user }))
-    .catch((err) => {
-      if (err) {
-        res.status(400).send({ message: "invalid user data" - `${err}` });
-      } else {
-        res.status(500).send({ Message: "internal error" });
-      }
-    });
-});
-// Route to delete a post
-
-// Route for like
-app.patch("/api/update/:username", async (req, res) => {
-  const username = req.params.username;
-  const data = req.body;
-  await updateUser(data, username)
-    .then((user) => res.send({ data: user }))
-    .catch((err) => {
-      if (err) {
-        res.status(400).send({ message: "invalid user data" - `${err}` });
-      } else {
-        res.status(500).send({ Message: "internal error" });
-      }
-    });
-});
-// Route to delete a post
-
-app.delete("/api/delete/:id", async (req, res) => {
-  const id = req.params.id;
-  await deleteUser(id)
-    .then((user) => res.send({ data: user }))
-    .catch((err) => {
-      if (err) {
-        res.status(400).send({ message: "invalid user data" - `${err}` });
-      } else {
-        res.status(500).send({ Message: "internal error" });
-      }
     });
 });
 
